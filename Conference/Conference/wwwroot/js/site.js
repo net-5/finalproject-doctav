03426babefaea888e1ea5d99c9f16789c30d29fe
doctav <doctav@uaic.ro>
2019-10-05 12:03:17 +0300

    Project in progress. With help from Karim.

--- Conference/Conference/wwwroot/js/site.js
+++ Conference/Conference/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//from Super KARIM
+var countDownDate = new Date("Nov 22, 2019 ").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    document.getElementById("demo").innerHTML = days + " DAYS TO THE EVENT (according to Karim's script :)";
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
